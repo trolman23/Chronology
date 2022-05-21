ab7d37fb0f6191d93b1df649a174660f2d28277a
TheNanoMachi <anthonyl99087@gmail.com>
2022-05-21 15:01:53 -0400

    completed markdown editor

--- public/main.js
+++ public/main.js
@@ -2,8 +2,20 @@ const editor = document.querySelector(".editor");
 const preview = document.querySelector(".preview");
 const converter = new showdown.Converter();
 
+const render = (value) => {
+    const html = converter.makeHtml(value);
+    preview.innerHTML = html; 
+}
+window.sessionStorage.setItem('md', editor.value)
 editor.addEventListener("keyup", evt => {
     const {value} = evt.target;
-    const html = converter.makeHtml(value);
-    preview.innerHTML = html;
+    window.sessionStorage.setItem('md', value);
+    render(value);
 });
+
+const storedMd = window.sessionStorage.getItem('md');
+
+if(storedMd) {
+    editor.value = storedMd;
+    render(editor.value);
+}
